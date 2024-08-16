6bd0221c03a8c55d219e4146faa9021cc0f9c1ba
BORA HARSHA RAJ VENKATA SAI REDDY <88108358+Harsharajreddy@users.noreply.github.com>
2024-08-15 21:15:04 -0500

    Add files via upload

---
+++ precache-manifest.dc7c91b0f7b3447a92f13bc799b62e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1315ba8bfc73de50f0026a465acb4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbbb5fd3e3656458741e",
+    "url": "/static/css/main.020e2537.chunk.css"
+  },
+  {
+    "revision": "751e1bf629144ba2b309",
+    "url": "/static/js/2.7c706a04.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.7c706a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbbb5fd3e3656458741e",
+    "url": "/static/js/main.2e84e8b8.chunk.js"
+  },
+  {
+    "revision": "7873725ba672e91c2fdf",
+    "url": "/static/js/runtime-main.deab8be9.js"
+  }
+]);
